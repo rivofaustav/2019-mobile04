18bca863302417a53d25906026a932f9abf904ab
rivofaustav <rivofaustav.rv@gmail.com>
2019-09-16 15:35:51 +0700

    do implicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
@@ -9,9 +9,20 @@ import android.widget.EditText;
 
 public class BundleActivity extends AppCompatActivity {
 
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY = "name";
+    public static final String AGE_KEY = "age";
+
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bundle);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
 }
